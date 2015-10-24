66c243c4d9083e48024e175ddabcbc36352a3066
rikki-a-p <rpennisi@pratt.edu>
2015-10-24 15:33:39 -0400

    IT Experience 2014-2015

---
+++ tagparser14_15.php
@@ -0,0 +1,63 @@
+<html>
+<body>
+<?php
+
+echo "<pre>";
+
+#open file for reading 
+	
+$fh = fopen("14_15.txt", 'r') or
+die("Could not open file for reading");
+
+$frequencies = array();
+
+while ($line = fgets($fh)) {
+	#echo $line."<br>";
+	$lineArray = explode(',', $line);
+	foreach ($lineArray as $value) {
+		# clean up each tag for storage
+		$lcValue = trim(strtolower($value));
+		#print "A value is: $lcValue<br>";
+		# put in associative array
+		if (isset($frequencies[$lcValue])) {
+			$frequencies[$lcValue]++;
+		}  else {
+			$frequencies[$lcValue]=1;
+		}
+	}
+
+}
+
+ksort($frequencies);
+
+fclose($fh);
+
+#  open a file for writing
+$fhw = fopen("14_15codes.txt", 'w') or die("Failed to create file");
+
+# loop through $frequencies array to print out the key and value, separated by a tab, one key/tab/value per line
+
+foreach ($frequencies as $value => $line){
+	
+$text = $value."\t".$line."\n";
+fwrite($fhw, $text) or die("Could not write to file");
+
+}
+
+
+
+# in your loop, write each of those lines to the writing file, slide 19 has an output file example
+# with a line like this fwrite($fhw, $text) or die("Could not write to file");
+
+
+
+# end the loop
+# close file
+fclose($fhw);
+
+echo "File '14_15codes.txt' written successfully";
+echo "</pre>";
+
+?>
+</body>
+</html>
